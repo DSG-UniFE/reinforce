a253d4eeedb3d71a73c89e14fb33515ee23b61e4
Filippo Poltronieri <filippopoltronieri@MBP-di-Filippo.lan>
2024-04-29 21:39:54 +0200

    Gridworld testing modified

--- examples/dqn_gridworld_test.rb
+++ examples/dqn_gridworld_test.rb
@@ -14,18 +14,30 @@ start = [0, 0]
 goal = [size - 1, size - 1]
 obstacles = 5
 environment = Reinforce::Environments::GridWorld.new(size, start, goal, obstacles)
-
+state_size = environment.state_size
+num_actions = environment.actions.size
 # Parameters
 learning_rate = 0.01
 discount_factor = 0.7
 max_actions_per_episode = 150
 
+architectureq = Torch::NN::Sequential.new(
+          Torch::NN::Linear.new(state_size, 128),
+          Torch::NN::ReLU.new,
+          Torch::NN::Linear.new(128, 128),
+          Torch::NN::ReLU.new,
+          Torch::NN::Linear.new(128, num_actions)
+        )
 
-# Create the agent
-agent = Reinforce::Algorithms::DQN.new(environment, learning_rate, discount_factor)
+q_function_model = Reinforce::QFunctionANN.new(environment.state_size, environment.actions.size, 
+                                              learning_rate, discount_factor, architecture: architectureq)
 
-# Load the agent
+# We do not need to load the target network because when testing
 
+agent = Reinforce::Algorithms::DQN.new(environment, learning_rate, discount_factor, 
+                                       q_function_model: q_function_model, q_function_model_target: nil)
+
+# Load the agent parameters
 agent.load('gridworld_dqn.pth')
 
 # Print the learned policy
@@ -34,12 +46,12 @@ test_episodes = 100
 accomplished = 0
 test_episodes.times do 
   state = environment.reset 
-  max_actions_per_episode.times do
+  max_actions_per_episode.times do |s|
     action = agent.predict(state) 
     #warn "State: #{state} action: #{action}"
     state, _, done = environment.step(action.to_i)
     #environment.render($stdout)
-    if done
+    if done && s < max_actions_per_episode - 1
       warn 'Goal reached!'
       accomplished += 1
       break

ea9e00035c24f237e5e9261cb3b16220d36e4bcb
Mauro Tortonesi <mauro.tortonesi@unife.it>
2026-08-31 16:46:59 +0200

    Revised gems.rb and removed gems.locked.

--- gems.rb
+++ gems.rb
@@ -1,21 +1,25 @@
 # frozen_string_literal: true
 
-# Released under the MIT License.
-# Copyright, 2023, by Mauro Tortonesi.
-
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in reinforce.gemspec
 gemspec
 
-gem 'bake', '>= 0.25.0'
-gem 'bake-gem', '>= 0.14.0'
-gem 'bake-modernize', '>= 0.57.1'
-gem 'ostruct', '>= 0.6.3' # no longer included in Ruby, so we need to explicitly add it as a dependency; required by standard
-gem 'tsort', '>= 0.2.0' # no longer included in Ruby, so we need to explicitly add it as a dependency; required by standard
-gem 'standard', '>= 1.56'
+
+group :examples do
+  gem 'unicode_plot', '>= 0.0.5'
+end
 
 group :test do
   gem 'covered', '>= 0.30.0'
   gem 'sus', '>= 0.37.2'
+end
+
+group :development do
+  gem 'bake', '>= 0.25.0'
+  gem 'bake-gem', '>= 0.14.0'
+  gem 'bake-modernize', '>= 0.57.1'
+  gem 'ostruct', '>= 0.6.3' # no longer included in Ruby, so we need to explicitly add it as a dependency; required by standard
+  gem 'tsort', '>= 0.2.0' # no longer included in Ruby, so we need to explicitly add it as a dependency; required by standard
+  gem 'standard', '>= 1.56'
 end
